874c6e4c61b323f5c3ee1d2a092658cf5cd38364
bfren <bf@bfren.dev>
2022-08-18 20:02:49 +0100

    Using Roboto by default

--- pages/parts/header.php
+++ pages/parts/header.php
@@ -19,6 +19,11 @@ $links = array(
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?> | Church Suite Feeds</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style type="text/css">
+        :root {
+            --bs-font-sans-serif: -apple-system,Roboto,"Helvetica Neue","Noto Sans","Liberation Sans",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        }
+    </style>
 </head>
 
 <body class="d-flex flex-column h-100">
