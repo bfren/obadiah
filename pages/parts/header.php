dbca9aa13e792ab6c819f2f3a02151c3010c263d
bfren <bf@bfren.dev>
2022-08-18 20:24:49 +0100

    Using local css / js

--- pages/parts/header.php
+++ pages/parts/header.php
@@ -18,7 +18,7 @@ $links = array(
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?> | Church Suite Feeds</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
     <style type="text/css">
         :root {
             --bs-font-sans-serif: -apple-system,Roboto,"Helvetica Neue","Noto Sans","Liberation Sans",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
