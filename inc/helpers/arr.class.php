8c7be4cc7ed1805fb29028db8bc2ca743707f492
bfren <bf@bfren.dev>
2022-08-20 11:49:13 +0100

    Default default value if not present in array

--- inc/helpers/arr.class.php
+++ inc/helpers/arr.class.php
@@ -11,14 +11,15 @@ class Arr
      *
      * @param array $array              Array of values.
      * @param string $key               The key to search for.
+     * @param mixed $default            Default value if key does not exist.
      * @return mixed                    Key value, or empty string if key is not found.
      */
-    public static function get(array $array, string $key): mixed
+    public static function get(array $array, string $key, mixed $default = null): mixed
     {
-        if (array_key_exists($key, $array)) {
+        if (array_key_exists($key, $array) && $array[$key]) {
             return $array[$key];
         }
 
-        return null;
+        return $default;
     }
 }
