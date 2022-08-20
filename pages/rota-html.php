70ba4909648a247c135b790019e7cfd41af5e51c
bfren <bf@bfren.dev>
2022-08-20 16:49:20 +0100

    Improving labels and layout of rota filters

--- pages/rota-html.php
+++ pages/rota-html.php
@@ -20,53 +20,65 @@ require_once("parts/header.php"); ?>
 <h2 class="border-bottom">Filters</h2>
 <form method="GET" action="/rota/">
     <div class="row mb-2">
-        <label for="person" class="col-2 col-form-label">Person</label>
-        <div class="col-8 col-md-9">
-            <select class="form-control" name="person">
-                <option value="">Select a person</option>
-                <?php foreach ($rota->people as $person) : $selected = $person == Arr::get($filters, "person") ? "selected" : ""; ?>
-                    <option value="<?php echo $person; ?>" <?php echo $selected; ?>><?php echo $person; ?></option>
-                <?php endforeach; ?>
-            </select>
+        <div class="col-8 col-sm-6">
+            <div class="input-group ">
+                <span class="input-group-text" for="person">Person</span>
+                <select class="form-control" name="person">
+                    <option value="">Choose...</option>
+                    <?php foreach ($rota->people as $person) : $selected = $person == Arr::get($filters, "person") ? "selected" : ""; ?>
+                        <option value="<?php echo $person; ?>" <?php echo $selected; ?>><?php echo $person; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
         </div>
-        <div class="col-2 col-md-1 d-flex align-items-center">
-            <div class="form-check">
+        <div class="col-4 col-sm-6 d-flex align-items-end">
+            <div class="form-check ">
                 <?php $checked = Arr::get($filters, "include") == "all" ? "checked" : ""; ?>
                 <input class="form-check-input" type="checkbox" value="all" name="include" id="include" <?php echo $checked; ?> />
-                <label class="form-check-label" for="include">All</label>
+                <label class="form-check-label" for="include">Include All</label>
             </div>
         </div>
     </div>
     <div class="row mb-2">
-        <label for="from" class="col-2 col-form-label">Dates</label>
-        <div class="col-5">
-            <input type="date" class="form-control" name="from" placeholder="From" value="<?php echo Arr::get($filters, "from"); ?>" />
+        <div class="col-6">
+            <div class="input-group ">
+                <span class="input-group-text" for="from">From</span>
+                <input type="date" class="form-control" name="from" id="from" placeholder="From" value="<?php echo Arr::get($filters, "from"); ?>" />
+            </div>
         </div>
-        <div class="col-5">
-            <input type="date" class="form-control" name="to" placeholder="To" value="<?php echo Arr::get($filters, "to"); ?>" />
+        <div class="col-6">
+            <div class="input-group ">
+                <span class="input-group-text" for="to">To</span>
+                <input type="date" class="form-control" name="to" id="to" placeholder="To" value="<?php echo Arr::get($filters, "to"); ?>" />
+            </div>
         </div>
     </div>
     <div class="row mb-2">
-        <label for="day" class="col-2 col-form-label">Day</label>
-        <div class="col-5">
-            <select class="form-control" name="day">
-                <option value="">Select a day of the week</option>
-                <?php foreach (Builder::$days_of_the_week as $num => $txt) :  $selected = $num == Arr::get($filters, "day") ? "selected" : ""; ?>
-                    <option value="<?php echo $num; ?>" <?php echo $selected; ?>><?php echo $txt; ?></option>
-                <?php endforeach; ?>
-            </select>
+        <div class="col-6">
+            <div class="input-group ">
+                <span class="input-group-text" for="day">Day</span>
+                <select class="form-control" name="day">
+                    <option value="">Choose...</option>
+                    <?php foreach (Builder::$days_of_the_week as $num => $txt) :  $selected = $num == Arr::get($filters, "day") ? "selected" : ""; ?>
+                        <option value="<?php echo $num; ?>" <?php echo $selected; ?>><?php echo $txt; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
         </div>
-        <div class="col-5">
-            <input type="time" class="form-control" name="start" placeholder="Start" value="<?php echo Arr::get($filters, "start"); ?>" />
+        <div class="col-6">
+            <div class="input-group ">
+                <span class="input-group-text" for="time">Time</span>
+                <input type="time" class="form-control" name="start" placeholder="Start" value="<?php echo Arr::get($filters, "start"); ?>" />
+            </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-2"></div>
-        <div class="col-10">
-            <button type="submit" class="btn btn-primary">Apply</button>
-            <a href="/rota/" class="btn btn-danger">Reset</a>
-            <a href="/rota/ics/?<?php echo $_SERVER["QUERY_STRING"]; ?>&api=<?php echo C::$login->api ?>" class="btn btn-secondary ms-3" target="_blank">ICS</a>
-            <a href="/rota/json/?<?php echo $_SERVER["QUERY_STRING"]; ?>&api=<?php echo C::$login->api ?>" class="btn btn-secondary d-none d-sm-inline-block" target="_blank">JSON</a>
+        <div class="col-6"></div>
+        <div class="col-6 d-flex justify-content-end">
+            <button type="submit" class="btn btn-primary me-1">Apply</button>
+            <a href="/rota/" class="btn btn-danger me-3">Reset</a>
+            <a href="/rota/ics/?<?php echo $_SERVER["QUERY_STRING"]; ?>&api=<?php echo C::$login->api ?>" class="btn btn-secondary me-1" target="_blank">ICS</a>
+            <a href="/rota/json/?<?php echo $_SERVER["QUERY_STRING"]; ?>&api=<?php echo C::$login->api ?>" class="btn btn-secondary d-none d-sm-inline-block me-1" target="_blank">JSON</a>
             <a href="/rota/print/?<?php echo $_SERVER["QUERY_STRING"]; ?>" class="btn btn-secondary d-none d-sm-inline-block" target="_blank">Print</a>
         </div>
     </div>
