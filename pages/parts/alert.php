2bba3f4ca91981f3ef33320398863d585edb6ac6
bfren <bf@bfren.dev>
2022-08-24 11:22:16 +0100

    Adding margin to top of alerts

--- pages/parts/alert.php
+++ pages/parts/alert.php
@@ -11,5 +11,5 @@ App::check();
 ?>
 
 <?php if (isset($result)) : $alert = $result->success ? "success" : "warning"; ?>
-    <div class="alert alert-<?php echo $alert; ?>"><?php echo $result->message; ?></div>
+    <div class="alert alert-<?php echo $alert; ?> mt-2"><?php echo $result->message; ?></div>
 <?php endif; ?>
