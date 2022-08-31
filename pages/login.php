03b1193ce46fb315c39885870836ed28f0151441
bfren <bf@bfren.dev>
2022-08-31 10:48:44 +0100

    Adding username field to login

--- pages/login.php
+++ pages/login.php
@@ -11,13 +11,14 @@ App::check();
 
 // handle post requests
 if (Request::$method == "POST") {
+    $user = Arr::get($_POST, "username");
     $pass = Arr::get($_POST, "password");
     // check password and redirect to home if it is correct
     // if it is not unset auth variable and increment count
-    if ($pass == C::$login->pass) {
+    if ($user == "user" && $pass == C::$login->pass) {
         Request::authorise();
         Request::redirect("/");
-    } elseif ($pass == C::$login->admin) {
+    } elseif ($user == "admin" && $pass == C::$login->admin) {
         Request::authorise(true);
         Request::redirect("/upload");
     } else {
@@ -34,9 +35,14 @@ require_once("parts/header.php");
 
 ?>
 
-<p class="mt-2">Please enter the password to access.</p>
+<p class="mt-2">Please log in to access.</p>
 
 <form class="row row-cols-lg-auto g-3 align-items-center needs-validation" method="POST" novalidate>
+    <div class="col-12 position-relative">
+        <label class="visually-hidden" for="password">Username</label>
+        <input type="text" class="form-control" name="username" id="usernam" placeholder="Username" required />
+        <div class="invalid-tooltip">Please enter the username.</div>
+    </div>
     <div class="col-12 position-relative">
         <label class="visually-hidden" for="password">Password</label>
         <input type="password" class="form-control" name="password" id="password" placeholder="Password" required />
