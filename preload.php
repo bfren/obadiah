7cd72c26a7870fbbe67a95e900ac512ce5ac6f44
bfren <bf@bfren.dev>
2022-09-02 09:20:01 +0100

    Adding preload script to be called as a regular maintenance task

---
+++ preload.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Feeds;
+
+use Feeds\Cache\Cache;
+use Feeds\Json\Json;
+use Feeds\Lectionary\Lectionary;
+use Feeds\Prayer\Prayer_Calendar;
+use Feeds\Rota\Rota;
+
+// initialise app
+require_once("classes/app.class.php");
+App::init(__DIR__);
+
+// preload caches
+Cache::get_lectionary(fn () => new Lectionary(), true);
+Cache::get_prayer_calendar(fn () => new Prayer_Calendar(), true);
+Cache::get_rota(fn () => new Rota(), true);
+
+// output simple respone
+Json::output("OK");
