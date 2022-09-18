df92c5f05a8ee74df65823f04f5ec38349b0a7cb
bfren <bf@bfren.dev>
2022-09-18 15:08:26 +0100

    Using action for refresh format

--- src/pages/refresh.php
+++ src/pages/refresh.php
@@ -4,21 +4,18 @@ namespace Feeds\Pages;
 
 use Feeds\App;
 use Feeds\Cache\Cache;
-use Feeds\Config\Config as C;
-use Feeds\Request\Request;
 
 App::check();
 
 // get refresh
 $refresh = Cache::get_refresh();
 
-// get format script
-$format = match (Request::$get->string("format")) {
+// get action page
+$action_page = match($action) {
     "ics" => "refresh-ics.php",
     "json" => "refresh-json.php",
     default => "login.php"
 };
-$path = sprintf("%s/pages/%s", C::$dir->cwd, $format);
 
-// load format page
-require_once $path;
+// load action page
+require_once $action_page;
