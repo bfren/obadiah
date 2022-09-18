71fa527288b542d94aa80efaeed13e84cf89fdc6
bfren <bf@bfren.dev>
2022-09-18 09:15:37 +0100

    Adding html echo functions

--- src/classes/helpers/escape.class.php
+++ src/classes/helpers/escape.class.php
@@ -11,31 +11,64 @@ class Escape
     /**
      * Escape $text - using sprintf if $args are defined.
      *
-     * @param string $text                  Text (or sprintf format) to be printed.
+     * @param null|string $text             Text (or sprintf format) to be escaped.
      * @param array $args                   Optional arguments to use for sprintf.
      * @return string                       Safe (escaped) text.
      */
-    public static function get(string $text, mixed ...$args): string
+    public static function get_text(?string $text, mixed ...$args): string
     {
+        // if string is null return a blank string
+        if ($text === null) {
+            return "";
+        }
+
         // if arguments have been provided, use sprintf
         if (count($args) > 0) {
             $formatted = sprintf($text, ...$args);
-            return _e($formatted);
+            return self::get_text($formatted);
         }
 
-        // convert HTML characters so the text is safe are safe
-        return htmlspecialchars($text, ENT_QUOTES);
+        // convert HTML characters so the text is safe
+        return htmlspecialchars($text, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
     }
 
     /**
      * Safely echo $text - using sprintf if $args are defined.
      *
-     * @param string $text                  Text (or sprintf format) to be printed.
+     * @param null|string $text             Text (or sprintf format) to be printed.
      * @param array $args                   Optional arguments to use for sprintf.
      * @return void
      */
-    public static function echo(string $text, mixed ...$args): void
+    public static function echo_text(?string $text, mixed ...$args): void
     {
-        echo self::get($text, $args);
+        echo self::get_text($text, ...$args);
+    }
+
+    /**
+     * Escape $html.
+     *
+     * @param null|string $html             HTML to be escaped.
+     * @return string                       Safe (escaped) html.
+     */
+    public static function get_html(?string $html): string
+    {
+        // if html is null return a blank string
+        if ($html === null) {
+            return "";
+        }
+
+        // handle as HTML characters so the text is safe are safe
+        return $html;
+    }
+
+    /**
+     * Safely echo $html.
+     *
+     * @param null|string $html             HTML to be printed.
+     * @return void
+     */
+    public static function echo_html(?string $html): void
+    {
+        echo self::get_html($html);
     }
 }
--- src/functions/escape.php
+++ src/functions/escape.php
@@ -5,23 +5,34 @@ use Feeds\Helpers\Escape;
 /**
  * Escape $text - using sprintf if $args are defined.
  *
- * @param string $text                  Text (or sprintf format) to be printed.
+ * @param null|string $text             Text (or sprintf format) to be printed.
  * @param array $args                   Optional arguments to use for sprintf.
  * @return string                       Safe (escaped) text.
  */
-function __(string $text, mixed ...$args): string
+function __(?string $text, mixed ...$args): string
 {
-    return Escape::get($text, $args);
+    return Escape::get_text($text, ...$args);
 }
 
 /**
  * Safely echo $text - using sprintf if $args are defined.
  *
- * @param string $text                  Text (or sprintf format) to be printed.
+ * @param null|string $text             Text (or sprintf format) to be printed.
  * @param array $args                   Optional arguments to use for sprintf.
  * @return void
  */
-function _e(string $text, mixed ...$args): void
+function _e(?string $text, mixed ...$args): void
 {
-    Escape::echo($text, $args);
+    Escape::echo_text($text, ...$args);
+}
+
+/**
+ * Safely echo $html.
+ *
+ * @param null|string $html             HTML to be printed.
+ * @return void
+ */
+function _h(?string $html): void
+{
+    Escape::echo_html($html);
 }
