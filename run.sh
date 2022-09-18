9b8190b735b0487564d4a6f875086ab738b7e007
bfren <bf@bfren.dev>
2022-09-18 18:43:36 +0100

    Using ccf image for run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/usr/local/bin/fish
 
-docker pull bfren/nginx-php:php8.1
+docker pull bfren/ccf
 docker run -p "127.0.0.1:3000:80" \
-    -e NGINX_ROOT_OVERRIDE="/www/public" \
-    -e PHP_EXT="curl pecl-yaml session" \
     -v (pwd)/src:/www \
     -v (pwd)/data:/data \
-    bfren/nginx-php:php8.1
+    bfren/ccf
