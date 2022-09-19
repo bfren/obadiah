cff34dac17af067b7ce71b40541472ce31c5fa71
bfren <bf@bfren.dev>
2022-09-19 15:58:43 +0100

    Adding missing comments

--- src/pages/rota/index-model.class.php
+++ src/pages/rota/index-model.class.php
@@ -3,11 +3,24 @@
 namespace Feeds\Pages\Rota;
 
 use Feeds\App;
+use Feeds\Rota\Combined_Day;
 
 App::check();
 
 class Index_Model
 {
+    /**
+     * Create Index model.
+     *
+     * @param array $filters                Array of rota filters.
+     * @param array $ten_thirty             Sunday 10:30 service filter preset.
+     * @param array $wednesday              Wednesday Morning Prayer service filter preset.
+     * @param string[] $days_of_the_week    Array of the days of the week, starting with Sunday.
+     * @param array $people                 All the people in the rota.
+     * @param array $series                 All the series in the rota.
+     * @param Combined_Day[] $days          The days containing service information.
+     * @return void
+     */
     public function __construct(
         public readonly array $filters,
         public readonly array $ten_thirty,
--- src/pages/rota/print-model.class.php
+++ src/pages/rota/print-model.class.php
@@ -3,11 +3,20 @@
 namespace Feeds\Pages\Rota;
 
 use Feeds\App;
+use Feeds\Rota\Combined_Day;
 
 App::check();
 
 class Print_Model
 {
+    /**
+     *
+     * @param null|string $time         Optional time filter.
+     * @param null|string $day          Optional day filter.
+     * @param null|string $person       Optional person filter.
+     * @param Combined_Day[] $days      The days containing service information.
+     * @return void
+     */
     public function __construct(
         public readonly ?string $time,
         public readonly ?string $day,
