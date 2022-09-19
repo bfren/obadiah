e303a2891aee946679885c2d2c1e7a801b1abe3d
bfren <bf@bfren.dev>
2022-09-19 14:57:47 +0100

    Mapping new routes

--- src/public/index.php
+++ src/public/index.php
@@ -2,38 +2,26 @@
 
 namespace Feeds;
 
-use Feeds\Config\Config as C;
-use Feeds\Helpers\Arr;
-use Feeds\Request\Request;
-use Feeds\Response\Response;
 use Feeds\Router\Router;
-use SplFileInfo;
 
 // initialise app
 require_once "../app.class.php";
 App::init();
 
 // map routes
-Router::map("login", "Login");
+Router::map("Ajax");
+Router::map("Auth", requires_auth: false);
+Router::map("Events", requires_auth: false);
+Router::map("Prayer");
+Router::map("Preload", requires_auth: false);
+Router::map("Refresh");
+Router::map("Rota");
+Router::map("Services", requires_auth: false);
+Router::map("Upload", requires_admin: true);
 
-// check auth
-Request::$session->is_authorised || Response::redirect("/login.php", true);
+// dispatch route
+$action = Router::dispatch();
 
-// get requested page
-$uri = explode("/", Request::$uri);
-$parts = Arr::match($uri);
-$page = Arr::get($parts, 0, "home");
-$action = Arr::get($parts, 1, "index");
-
-$action = Router::dispatch($page, $action);
-print_r($view);
-$view->execute();exit;
-
-// output requested page, or home by default
-$path = sprintf("%s/pages/%s.php", C::$dir->cwd, $page);
-$file = new SplFileInfo($path);
-if (!$file->isFile()) {
-    $path = sprintf("%s/pages/home.php", C::$dir->cwd);
-}
-
-require_once $path;
+// execute action
+$action->send_headers();
+$action->execute();
