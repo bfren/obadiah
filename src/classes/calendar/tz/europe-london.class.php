5e11441be70e8071d00e78cd978001146befc4c7
bfren <bf@bfren.dev>
2022-09-21 13:02:33 +0100

    Simplifying array definition

--- src/classes/calendar/tz/europe-london.class.php
+++ src/classes/calendar/tz/europe-london.class.php
@@ -16,24 +16,24 @@ class Europe_London implements Timezone
      */
     public function get_definition(): array
     {
-        $definition = array();
-        $definition[] = "BEGIN:VTIMEZONE";
-        $definition[] = "TZID:Europe/London";
-        $definition[] = "BEGIN:STANDARD";
-        $definition[] = "TZNAME:GMT";
-        $definition[] = "DTSTART:19710101T020000";
-        $definition[] = "TZOFFSETFROM:+0100";
-        $definition[] = "TZOFFSETTO:+0000";
-        $definition[] = "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU";
-        $definition[] = "END:STANDARD";
-        $definition[] = "BEGIN:DAYLIGHT";
-        $definition[] = "TZNAME:BST";
-        $definition[] = "DTSTART:19710101T010000";
-        $definition[] = "TZOFFSETFROM:+0000";
-        $definition[] = "TZOFFSETTO:+0100";
-        $definition[] = "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU";
-        $definition[] = "END:DAYLIGHT";
-        $definition[] = "END:VTIMEZONE";
-        return $definition;
+        return array(
+            "BEGIN:VTIMEZONE",
+            "TZID:Europe/London",
+            "BEGIN:STANDARD",
+            "TZNAME:GMT",
+            "DTSTART:19710101T020000",
+            "TZOFFSETFROM:+0100",
+            "TZOFFSETTO:+0000",
+            "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU",
+            "END:STANDARD",
+            "BEGIN:DAYLIGHT",
+            "TZNAME:BST",
+            "DTSTART:19710101T010000",
+            "TZOFFSETFROM:+0000",
+            "TZOFFSETTO:+0100",
+            "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU",
+            "END:DAYLIGHT",
+            "END:VTIMEZONE"
+        );
     }
 }
