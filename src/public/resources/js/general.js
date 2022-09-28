c26dab820e10146890bed7058be077a8101a3d8d
bfren <bf@bfren.dev>
2022-09-28 11:03:26 +0100

    Adding comment

--- src/public/resources/js/general.js
+++ src/public/resources/js/general.js
@@ -1,9 +1,16 @@
+/**
+ * Copy some text to the clipboard.
+ *
+ * @param {string} text                 The text to copy
+ * @returns
+ */
 function copy(text) {
     navigator.clipboard.writeText(text)
         .then(() => {
-            alert("Copied " + text);
+            alert("Copied!");
         })
         .catch(() => {
             alert("Something went wrong - try copying this: " + text);
         });
+    return false;
 }
