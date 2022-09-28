5b0cacfb53a4115f763f6aba7411e0f09c09846e
bfren <bf@bfren.dev>
2022-09-28 11:54:37 +0100

    Switching ics hyperlink to be a button

--- src/public/resources/js/copy.js
+++ src/public/resources/js/copy.js
@@ -1,3 +1,12 @@
 var clipboard = new ClipboardJS(".copy");
 
+clipboard.on("success", function(e) {
+    console.info("Text copied:", e.text);
+    e.clearSelection();
+});
 
+clipboard.on("error", function(e) {
+    alert("Something went wrong, plese try copying manually: " + e.text);
+    console.error("Action:", e.action);
+    console.error("Trigger:", e.trigger);
+});
