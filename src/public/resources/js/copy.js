55f11de61cd2b63a98d90167d70d1b9e728d1ee1
bfren <bf@bfren.dev>
2022-09-28 11:47:57 +0100

    Adding user feedback on copy

--- src/public/resources/js/copy.js
+++ src/public/resources/js/copy.js
@@ -1,1 +1,18 @@
-new ClipboardJS(".copy");
+var clipboard = new ClipboardJS(".copy");
+
+clipboard.on("success", function(e) {
+    alert("Copied!");
+
+    console.info("Action:", e.action);
+    console.info("Text:", e.text);
+    console.info("Trigger:", e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on("error", function(e) {
+    alert("Something went wrong, plese try copying manually: " + e.text);
+
+    console.error("Action:", e.action);
+    console.error("Trigger:", e.trigger);
+});
