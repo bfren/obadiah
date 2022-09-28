a0205bcacb4b9dc05138137fcba83cbdd3c9a745
bfren <bf@bfren.dev>
2022-09-28 11:49:37 +0100

    Don't use alert after copy

--- src/public/resources/js/copy.js
+++ src/public/resources/js/copy.js
@@ -1,18 +1,12 @@
 var clipboard = new ClipboardJS(".copy");
 
 clipboard.on("success", function(e) {
-    alert("Copied!");
-
-    console.info("Action:", e.action);
-    console.info("Text:", e.text);
-    console.info("Trigger:", e.trigger);
-
+    console.info("Text copied:", e.text);
     e.clearSelection();
 });
 
 clipboard.on("error", function(e) {
     alert("Something went wrong, plese try copying manually: " + e.text);
-
     console.error("Action:", e.action);
     console.error("Trigger:", e.trigger);
 });
