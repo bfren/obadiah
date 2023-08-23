2d880598a886fd91aed1ae1e2977a8a79d623fde
bfren <bf@bfren.dev>
2023-08-23 16:11:26 +0100

    Moving contact office text to be at the bottom of each page - #61

--- src/pages/parts/footer/prayer-part.php
+++ src/pages/parts/footer/prayer-part.php
@@ -15,10 +15,9 @@ $today = new DateTimeImmutable("now", C::$events->timezone);
 </div><!-- primary container -->
 </main>
 
-<footer class="footer footer-prayer mt-2 py-1 bg-light">
+<footer class="footer footer-prayer mt-2">
     <div class="container-fluid">
         <small class="text-muted">
-            <p>These names come from our church address book. If you don&rsquo;t wish to be included, you can remove yourself in Church Suite or by contacting the office.</p>
             <p class="d-flex justify-content-between">
                 <span><a href="https://www.christchurchb29.org/privacy-policy" target="_blank">www.christchurchb29.org/privacy-policy</a></span>
                 <span>Generated on <?php _e($today->format("r")); ?></span>
