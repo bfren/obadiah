ae70df535fbae8ca9e84b5a192f5f1f13425896d
bfren <bf@bfren.dev>
2023-08-23 17:58:51 +0100

    Fixing header link to refresh page - #61

--- src/pages/refresh/heading-part.php
+++ src/pages/refresh/heading-part.php
@@ -14,12 +14,7 @@ App::check();
 ?>
 
 <h2 class="prayer-calendar-title row">
-    <div class="col-3"><a href="/prayer"><?php Image::echo_logo("logo me-4"); ?></a></div>
+    <div class="col-3"><a href="/refresh"><?php Image::echo_logo("logo me-4"); ?></a></div>
     <div class="col-6 text-center">Refresh Calendar</div>
     <div class="col-3 text-end"><?php _e($model); ?></div>
 </h2>
-
-<!-- <div class="prayer-calendar-verse">
-    Devote yourselves to prayer, being watchful and thankful.
-    <span class="ref">Colossians 4.2 (NIV)</span>
-</div> -->
