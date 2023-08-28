941a1fd5d3159a816ae96329b98cdd4875c86195
bfren <bf@bfren.dev>
2023-08-28 11:23:09 +0100

    Adding church name and domain to config - #75

--- src/classes/config/config-general.class.php
+++ src/classes/config/config-general.class.php
@@ -10,6 +10,27 @@ App::check();
 class Config_General
 {
     /**
+     * The name of your church (e.g. 'Christ Church').
+     *
+     * @var string
+     */
+    public readonly string $church_name;
+
+    /**
+     * The full name of your church (e.g. 'Christ Church Selly Park).
+     *
+     * @var string
+     */
+    public readonly string $church_name_full;
+
+    /**
+     * Your church's website domain name (do *not* include https://).
+     *
+     * @var string
+     */
+    public readonly string $church_domain;
+
+    /**
      * Whether or not the app is in production mode.
      *
      * @var bool
@@ -24,6 +45,9 @@ class Config_General
      */
     public function __construct(array $config)
     {
+        $this->church_name = Arr::get($config, "church_name", "");
+        $this->church_name_full = Arr::get($config, "church_name_full", "");
+        $this->church_domain = Arr::get($config, "church_domain", "");
         $this->production = Arr::get_boolean($config, "production", true);
     }
 }
