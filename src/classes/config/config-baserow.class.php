7a3399ac9367f68e1141c48758372d57fd97390f
bfren <bf@bfren.dev>
2023-09-21 11:41:45 +0100

    Adding table / view IDs to config - #98

--- src/classes/config/config-baserow.class.php
+++ src/classes/config/config-baserow.class.php
@@ -17,6 +17,34 @@ class Config_Baserow
     public readonly string $api_uri;
 
     /**
+     * The ID of the Day table.
+     *
+     * @var int
+     */
+    public readonly int $day_table_id;
+
+    /**
+     * The ID of the Day Feed view.
+     *
+     * @var int
+     */
+    public readonly int $day_view_id;
+
+    /**
+     * The ID of the Service table.
+     *
+     * @var int
+     */
+    public readonly int $service_table_id;
+
+    /**
+     * The ID of the Day feed view.
+     *
+     * @var int
+     */
+    public readonly int $service_view_id;
+
+    /**
      * Authorisation Token.
      *
      * @var string
@@ -32,6 +60,10 @@ class Config_Baserow
     public function __construct(array $config)
     {
         $this->api_uri = Arr::get($config, "api_uri", "");
+        $this->day_table_id = Arr::get_integer($config, "day_table_id");
+        $this->day_view_id = Arr::get_integer($config, "day_view_id");
+        $this->service_table_id = Arr::get_integer($config, "service_table_id");
+        $this->service_view_id = Arr::get_integer($config, "service_view_id");
         $this->token = Arr::get($config, "token", "");
     }
 }
