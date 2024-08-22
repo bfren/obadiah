649af61db21659acf72f9b717faf506edf3ac1ba
bfren <bf@bfren.dev>
2024-08-22 13:22:25 +0100

    Completing reference handling - #141

--- src/pages/safeguarding/safeguarding.class.php
+++ src/pages/safeguarding/safeguarding.class.php
@@ -5,6 +5,7 @@ namespace Obadiah\Pages\Safeguarding;
 use DateTimeImmutable;
 use Obadiah\App;
 use Obadiah\Baserow\Baserow;
+use Obadiah\Helpers\Arr;
 use Obadiah\Request\Request;
 use Obadiah\Response\Json;
 
@@ -44,37 +45,37 @@ class Safeguarding
 
         // map JSON to Baserow table fields
         $row = array(
-            "Name" => $form["name-1"],
-            "Role Applied For" => $form["text-1"],
-            "1." => $form["select-1"],
-            "1. Details" => $form["textarea-1"],
-            "2." => $form["select-2"],
-            "2. Details" => $form["textarea-2"],
-            "3." => $form["select-3"],
-            "3. Details" => $form["textarea-3"],
-            "4." => $form["select-4"],
-            "4. Details" => $form["textarea-4"],
-            "5." => $form["select-5"],
-            "6." => $form["select-6"],
-            "6. Details" => $form["textarea-5"],
-            "7." => $form["select-7"],
-            "7. Details" => $form["textarea-6"],
-            "8." => $form["select-8"],
-            "8. Details" => $form["textarea-7"],
-            "9." => $form["select-9"],
-            "10." => $form["select-10"],
-            "11." => $form["select-11"],
-            "12." => $form["select-12"],
-            "13." => $form["select-13"],
-            "13. Details" => $form["textarea-8"],
-            "14." => $form["select-14"],
-            "14. Details" => $form["textarea-9"],
-            "15." => $form["select-15"],
-            "15. Details" => $form["textarea-10"],
-            "16." => $form["select-16"],
-            "16. Details" => $form["textarea-11"],
-            "Declaration" => $form["name-2"],
-            "Overseas Consent" => $form["name-3"]
+            "Name" => Arr::get($form, "name-1"),
+            "Role Applied For" => Arr::get($form, "text-1"),
+            "1." => Arr::get($form, "select-1"),
+            "1. Details" => Arr::get($form, "textarea-1"),
+            "2." => Arr::get($form, "select-2"),
+            "2. Details" => Arr::get($form, "textarea-2"),
+            "3." => Arr::get($form, "select-3"),
+            "3. Details" => Arr::get($form, "textarea-3"),
+            "4." => Arr::get($form, "select-4"),
+            "4. Details" => Arr::get($form, "textarea-4"),
+            "5." => Arr::get($form, "select-5"),
+            "6." => Arr::get($form, "select-6"),
+            "6. Details" => Arr::get($form, "textarea-5"),
+            "7." => Arr::get($form, "select-7"),
+            "7. Details" => Arr::get($form, "textarea-6"),
+            "8." => Arr::get($form, "select-8"),
+            "8. Details" => Arr::get($form, "textarea-7"),
+            "9." => Arr::get($form, "select-9"),
+            "10." => Arr::get($form, "select-10"),
+            "11." => Arr::get($form, "select-11"),
+            "12." => Arr::get($form, "select-12"),
+            "13." => Arr::get($form, "select-13"),
+            "13. Details" => Arr::get($form, "textarea-8"),
+            "14." => Arr::get($form, "select-14"),
+            "14. Details" => Arr::get($form, "textarea-9"),
+            "15." => Arr::get($form, "select-15"),
+            "15. Details" => Arr::get($form, "textarea-10"),
+            "16." => Arr::get($form, "select-16"),
+            "16. Details" => Arr::get($form, "textarea-11"),
+            "Declaration" => Arr::get($form, "name-2"),
+            "Overseas Consent" => Arr::get($form, "name-3")
         );
 
         // create Baserow connection and execute request
@@ -93,27 +94,27 @@ class Safeguarding
         $form = Request::$json;
 
         // remove hyphens from select values
-        $get_select = fn (int $num) => str_replace("-", " ", $form["select-$num"]);
+        $get_select = fn (int $num) => str_replace("-", " ", Arr::get($form, "select-$num"));
 
         // map JSON to Baserow table fields
         $row = array(
-            "Applicant Full Name" => $form["name-1"],
-            "Relationship" => $form["text-1"],
-            "Known For" => $form["text-2"],
+            "Applicant Full Name" => Arr::get($form, "name-1"),
+            "Relationship" => Arr::get($form, "text-1"),
+            "Known For" => Arr::get($form, "text-2"),
             "Suitability" => $get_select(1),
             "Experience" => $get_select(2),
             "Care" => $get_select(3),
             "Equality" => $get_select(4),
             "Honesty etc" => $get_select(5),
-            "Comments" => $form["textarea-1"],
-            "Health" => $form["select-6"],
-            "Health Details" => $form["textarea-2"],
-            "Unsuitability" => $form["select-7"],
-            "Unsuitability Details" => $form["textarea-3"],
-            "Referee Full Name" => $form["name-2"],
-            "Confirm" => $form["checkbox-1"] == "true",
-            "Referee Email" => $form["email-1"],
-            "Referee Phone" => $form["phone-1"],
+            "Comments" => Arr::get($form, "textarea-1"),
+            "Health" => Arr::get($form, "select-6"),
+            "Health Details" => Arr::get($form, "textarea-2"),
+            "Unsuitability" => Arr::get($form, "select-7"),
+            "Unsuitability Details" => Arr::get($form, "textarea-3"),
+            "Referee Full Name" => Arr::get($form, "name-2"),
+            "Confirm" => Arr::get($form, "checkbox-1") == "true",
+            "Referee Email" => Arr::get($form, "email-1"),
+            "Referee Phone" => Arr::get($form, "phone-1"),
         );
 
         // create Baserow connection and execute request
