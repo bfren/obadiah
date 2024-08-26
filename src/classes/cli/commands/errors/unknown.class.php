473255758d06635bfd500606c7aa45b9908829a9
bfren <bf@bfren.dev>
2024-08-26 14:21:34 +0100

    Readding commands directory - #153

---
+++ src/classes/cli/commands/errors/unknown.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Obadiah\Cli\Commands\Errors;
+
+use Obadiah\App;
+use Obadiah\Cli\Command;
+
+App::check();
+
+class Unknown extends Command
+{
+    /**
+     * Executed when a command has been called and is not mapped to an implementation class.
+     *
+     * @param string $command_name          Command name.
+     * @return void
+     */
+    public function __construct(
+        public readonly string $command_name
+    ) {}
+
+    /**
+     * Output error message and exit.
+     *
+     * @return never
+     */
+    public function execute(): void
+    {
+        App::die("Unknown command: %s.", $this->command_name);
+    }
+}
