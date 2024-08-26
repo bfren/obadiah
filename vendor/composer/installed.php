e2fd77e222056d86a7d0c9682969ef7a05884b4a
bfren <bf@bfren.dev>
2024-08-26 09:18:52 +0100

    Adding PHPStan - #150

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '2.0.0.x-dev',
+        'version' => '2.0.0.9999999-dev',
+        'reference' => 'c2ef63160eb9f544c4bc04dc5448297cd7a91f88',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '2.0.0.x-dev',
+            'version' => '2.0.0.9999999-dev',
+            'reference' => 'c2ef63160eb9f544c4bc04dc5448297cd7a91f88',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.11.11',
+            'version' => '1.11.11.0',
+            'reference' => '707c2aed5d8d0075666e673a5e71440c1d01a5a3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
