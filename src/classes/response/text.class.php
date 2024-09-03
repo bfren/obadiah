8873e4258f80d36d71c5a07bfc2a51284581d888
bfren <bf@bfren.dev>
2024-09-03 07:28:29 +0100

    Adding plain text response type - #172

---
+++ src/classes/response/text.class.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Obadiah\Response;
+
+use Obadiah\App;
+
+App::check();
+
+class Text extends Action
+{
+    /**
+     * Store plain text and add headers.
+     *
+     * @param mixed $text               Plain text value.
+     * @param int $status               HTTP status code.
+     * @param int|null $last_modified   Optional last modified timestamp.
+     * @return void
+     */
+    public function __construct(
+        public readonly string $text,
+        int $status = 200,
+        ?int $last_modified = null
+    ) {
+        // add default headers
+        parent::__construct($status);
+
+        // add debug headers
+        if ($this->add_debug_headers()) {
+            return;
+        }
+
+        // add standard plain text headers
+        $this->add_header("Content-Type", "text/plain; charset=utf-8");
+        $this->add_last_modified_header($last_modified);
+    }
+
+    /**
+     * Execute the action, encoding and printing the plain text value.
+     *
+     * @return void
+     */
+    public function execute(): void
+    {
+        print_r($this->text);
+    }
+}
