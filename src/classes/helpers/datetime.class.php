b2d8ac3ee3bacc0e7950a4e2c3b0fb297aecc813
bfren <bf@bfren.dev>
2025-02-21 10:20:09 +0000

    Adding get_interval_spec() helper - #203

--- src/classes/helpers/datetime.class.php
+++ src/classes/helpers/datetime.class.php
@@ -2,6 +2,7 @@
 
 namespace Obadiah\Helpers;
 
+use DateInterval;
 use DateTimeImmutable;
 use Exception;
 use Obadiah\App;
@@ -32,6 +33,45 @@ class DateTime
 
         // return
         return $dt;
+    }
+
+    /**
+     * Get a DateInterval spec string from a DateInterval object.
+     * H/T Slava Fomin II https://stackoverflow.com/a/25371691.
+     *
+     * @param DateInterval $interval        DateInterval object.
+     * @return string                       Interval spec string.
+     */
+    public static function get_interval_spec(DateInterval $interval): string
+    {
+        // reading all non-zero date parts
+        $date = array_filter([
+            "Y" => $interval->y,
+            "M" => $interval->m,
+            "D" => $interval->d
+        ]);
+
+        // reading all non-zero time parts
+        $time = array_filter([
+            "H" => $interval->h,
+            "M" => $interval->i,
+            "S" => $interval->s
+        ]);
+
+        $spec = "P";
+
+        // adding each part to the spec-string
+        foreach ($date as $key => $value) {
+            $spec .= $value . $key;
+        }
+        if (count($time) > 0) {
+            $spec .= "T";
+            foreach ($time as $key => $value) {
+                $spec .= $value . $key;
+            }
+        }
+
+        return $spec;
     }
 
     /**
