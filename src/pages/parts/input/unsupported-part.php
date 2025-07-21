16b18bc02b1d45e26fd0f4dfdbaa194f39b0b21e
bfren <bf@bfren.dev>
2025-07-21 18:12:34 +0100

    Adding 'unsupported' form part - #218

---
+++ src/pages/parts/input/unsupported-part.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Obadiah\Pages\Parts\Input;
+
+use Obadiah\App;
+use Obadiah\Response\View;
+
+App::check();
+
+/** @var View $this */
+/** @var Input_Model<string> $model */
+
+?>
+
+<div class="form-text">This setting can only be edited in <code>config.yml</code>.</div>
