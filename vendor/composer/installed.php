29b0b21f806f9bd7233cf6f9d5c721793b9a06f2
bfren <bf@bfren.dev>
2025-10-17 12:54:01 +0100

    Updating PHPStan - #242

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'c8e498c90f2eec1b86ac08504dd352f68fedd66f',
+        'pretty_version' => '2.3.0.x-dev',
+        'version' => '2.3.0.9999999-dev',
+        'reference' => '6419b957a9c88d8986c5304e4f34dea5ce5902f4',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'c8e498c90f2eec1b86ac08504dd352f68fedd66f',
+            'pretty_version' => '2.3.0.x-dev',
+            'version' => '2.3.0.9999999-dev',
+            'reference' => '6419b957a9c88d8986c5304e4f34dea5ce5902f4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'phpstan/phpstan' => array(
-            'pretty_version' => '2.1.22',
-            'version' => '2.1.22.0',
-            'reference' => '41600c8379eb5aee63e9413fe9e97273e25d57e4',
+            'pretty_version' => '2.1.31',
+            'version' => '2.1.31.0',
+            'reference' => 'ead89849d879fe203ce9292c6ef5e7e76f867b96',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpstan/phpstan',
             'aliases' => array(),
