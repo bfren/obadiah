a7c587e346154cf6bf9f211a600ad7463bde79d0
bfren <bf@bfren.dev>
2025-12-06 11:53:12 +0000

    Using new environment variable - #243

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 docker pull bfren/obadiah:dev
 docker run -it \
     -p "127.0.0.1:3000:80" \
-    -e PHP_INI=development \
+    -e BF_PHP_ENV=development \
     -v $(pwd)/src:/www \
     -v $(pwd)/data:/data \
     bfren/obadiah:dev nu
