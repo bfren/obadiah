25e82ce1ad2e3253455b2ce347832fd4a5412a4f
bfren <bf@bfren.dev>
2026-03-02 11:02:06 +0000

    Adding missing PHPDoc types - #260

--- src/classes/request/file.class.php
+++ src/classes/request/file.class.php
@@ -13,14 +13,14 @@ class File
      * Construct an object representing an uploaded file.
      *
      * @param string $original_name             The original name of the file on the client machine.
-     * @param ?string $mime_type                The mime type of the file, if the browser provided this information.
+     * @param null|string $mime_type            The mime type of the file, if the browser provided this information.
      * @param int $size_in_bytes                The size, in bytes, of the uploaded file.
      * @param string $tmp_name                  The temporary filename of the file in which the uploaded file was stored on the server.
      * @param null|string $error                The error associated with this file upload, or null on success.
      */
     public function __construct(
         public readonly string $original_name,
-        public readonly string $mime_type,
+        public readonly ?string $mime_type,
         public readonly int $size_in_bytes,
         public readonly string $tmp_name,
         public readonly ?string $error
@@ -30,7 +30,7 @@ class File
     /**
      * Create File object from $_FILES input array.
      *
-     * @param array $input                      Input array from $_FILES object.
+     * @param mixed[] $input                    Input array from $_FILES object.
      * @return File                             File object.
      */
     public static function create(array $input): self
@@ -44,14 +44,20 @@ class File
         );
     }
 
+    /**
+     * Text representations of file upload error codes.
+     * See https://www.php.net/manual/en/features.file-upload.errors.php.
+     *
+     * @var array<null|string>
+     */
     private static array $errors = array(
         0 => null,
-        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-        3 => 'The uploaded file was only partially uploaded',
-        4 => 'No file was uploaded',
-        6 => 'Missing a temporary folder',
-        7 => 'Failed to write file to disk.',
-        8 => 'A PHP extension stopped the file upload.',
+        1 => "The uploaded file exceeds the upload_max_filesize directive in php.ini",
+        2 => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form",
+        3 => "The uploaded file was only partially uploaded",
+        4 => "No file was uploaded",
+        6 => "Missing a temporary folder",
+        7 => "Failed to write file to disk.",
+        8 => "A PHP extension stopped the file upload.",
     );
 }
